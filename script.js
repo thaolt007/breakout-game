970844352f40920b785020cb88844e3272017fc7
thaolt <thaolt117@gmail.com>
2018-01-21 17:28:33 +0700

    add brick and collistion detect

--- script.js
+++ script.js
@@ -6,15 +6,52 @@ var ctx = canvas.getContext('2d');
 
 var cvW = canvas.width;
 var cvH = canvas.height;
-var x = Math.ceil(Math.random()*cvW);
-var y = Math.ceil(Math.random()*cvH);
-var radius = 10;
-var dx = 2; var dy = -2;
+
+// paddle
 var paddleWidth = 100; var paddleHeight = 10;
 var paddleX = Math.ceil(Math.random()*cvW);
 var right = false; var left = false;
 var paddleMove = 5;
+// ball
+var dx = 2; var dy = -2; // hướng di chuyển: lên trên bên phải
+var x = paddleX + paddleWidth/2; // vị trí x của quả bóng
+var y = cvH - paddleHeight; // vị trí y của quả bóng
+var radius = 10;
+// brick
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickColumn = 8;
+var brickRow = 3;
+var brickTopOffset = 20;
+var brickLeftOffset = 25;
+var bricks = [];
 
+for(var r = 0; r < brickRow; r++) {
+    bricks[r] = [];
+    for(var c = 0; c < brickColumn; c++) {
+        bricks[r][c] = {x:0, y:0, state:1}; // state=1: vẫn còn; state=0: đã mất
+    }
+}
+
+function drawBricks() {
+    for(var r = 0; r < brickRow; r++) {
+        for(var c = 0; c < brickColumn; c++) {
+            var brickX = c * (brickWidth + brickPadding) + brickLeftOffset;
+            var brickY = r * (brickHeight + brickPadding) + brickTopOffset;
+            bricks[r][c].x = brickX;
+            bricks[r][c].y = brickY;
+            if (bricks[r][c].state) {
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = 'blue';
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+// Bắt bấm phím sang trái, sang phải
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
 
@@ -36,7 +73,7 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
-
+// Vẽ tấm ván ở dưới
 function drawPaddle() {
     ctx.beginPath();
     ctx.fillStyle = 'green';
@@ -46,7 +83,7 @@ function drawPaddle() {
 
 function isGameOver() {
     if(y + radius >= cvH) {
-        if (x + radius < paddleX || x > paddleX + paddleWidth){
+        if(x + radius < paddleX || x > paddleX + paddleWidth){
             return true;
         }else {
             y = cvH - paddleHeight - radius;
@@ -54,20 +91,36 @@ function isGameOver() {
     }
     return false;
 }
+
+function collisionDectect() {
+    for(var r = 0; r < brickRow; r++) {
+        for(var c = 0; c < brickColumn; c++) {
+            if(bricks[r][c].state) {
+                if(x >= bricks[r][c].x && x <= bricks[r][c].x + brickWidth &&
+                    y-radius >= bricks[r][c].y && y-radius <= bricks[r][c].y + brickHeight) {
+                    dy = -dy;
+                    bricks[r][c].state = 0;
+                }
+            }
+        }
+    }
+}
 function draw() {
     ctx.clearRect(0, 0, cvW, cvH);
     drawBall();
     drawPaddle();
+    drawBricks();
     x += dx;
     y += dy;
-
+    collisionDectect();
+    // Chuyển hướng khi đến các cạnh hình hộp
     if(x + radius >= cvW || x - radius <= 0) {
         dx = -dx;
     }
     if(y + radius >= cvH || y - radius <= 0) {
         dy = -dy;
     }
-
+    // Cập nhật vị trí tấm ván khi bấm phím trái, phải
     if(right && paddleX + paddleWidth < cvW) paddleX += paddleMove;
     else if(paddleX + paddleWidth > cvW) paddleX = cvW - paddleWidth;
 
